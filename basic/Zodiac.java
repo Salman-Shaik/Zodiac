8433062d8b6e66ecf43e030e5e5b8b7afbbfcd8e
salman shaik <dukesalman5@gmail.com>
2018-03-16 16:59:22 +0530

    added a functionality for one zodiac sign

--- basic/Zodiac.java
+++ basic/Zodiac.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class Zodiac {
 private String rawDate;
-private Date dateOfBirth;
+private Date dateOfBirth,starts,ends;
+boolean condition;
 public Zodiac(String dob){this.rawDate=dob;}
 
 public int[] convertToIntegers(List<Integer> arrayList){
@@ -18,12 +19,27 @@ private int[] getDividedDate(){
   for (int i=0; i<fields.length; i++) date.add(Integer.parseInt(fields[i]));
   return convertToIntegers(date);
 }
+private boolean isAfter(){return starts.after(dateOfBirth);}
+private boolean isBefore(){return ends.after(dateOfBirth);}
+private boolean isEqual(){
+  return starts.equals(dateOfBirth)||ends.equals(dateOfBirth);
+}
+private boolean isSameOrInBetween(){return isAfter()&&isBefore()||isEqual();}
+
+private boolean isAries(){
+  int year = dateOfBirth.getYear();
+  starts=new Date(year,02,21);
+  ends=new Date(year,03,19);
+  if(isSameOrInBetween()) return true;
+  return false;
+}
 
 public String getSign(){
   int[] date = getDividedDate();
   if(date.length!=3) return "Invalid Date";
-  dateOfBirth= new Date(date[2],date[1],date[0]);
-  return "Aries";
+  dateOfBirth= new Date(date[2],date[1]-1,date[0]);
+  if(isAries()) return "Aries";
+  return "Cancer";
 }
 
 }
